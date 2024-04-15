4bc6c901baba539b549ac97590239ac198e3a95b
Sébastien VIAL <sebastien@shyrogan.fr>
2024-04-15 18:46:29 +0200

    feat: single location read route

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -6,13 +6,13 @@ import { isAuthenticated, validate } from "../utils/middlewares"
 import { Location } from "../model/location"
 import { Review } from "../model/review"
 
-async function createOffer(req: Request<TokenClaims>, res: Response) {
+async function create(req: Request<TokenClaims>, res: Response) {
   const offer = req.body as LocationType
   offer.owner = req.auth!!.sub
   return res.status(200).send(await Location.create(offer))
 }
 
-async function listOffer(req: Request<TokenClaims>, res: Response) {
+async function locations(req: Request<TokenClaims>, res: Response) {
   let filter = {}
   if (req.query.city) {
     filter = { city: { $regex: req.query.city }, ...filter }
@@ -33,11 +33,24 @@ async function listOffer(req: Request<TokenClaims>, res: Response) {
   return res.status(200).send(locations)
 }
 
+async function location(req: Request<TokenClaims>, res: Response) {
+  let id = req.params.id
+  const location = await Location.findById(id).lean()
+  if (!location) return res.status(404).send()
+  const rates = (await Review.find({ location: location?._id })).map(
+    (r) => r.rate,
+  )
+  if (rates.length > 0)
+    location.rate = rates.reduce((a, b) => a + b, 0) / rates.length
+  return res.status(200).send()
+}
+
 export const locationRouter = Router()
 locationRouter.post(
-  "/offers/create",
+  "/location/create",
   validate(LocationZodSchema.omit({ owner: true })),
   isAuthenticated,
-  createOffer,
+  create,
 )
-locationRouter.get("/offers", listOffer)
+locationRouter.get("/locations", locations)
+locationRouter.get("/location/:id", location)
--- src/routes/research.ts
+++ src/routes/research.ts
@@ -7,33 +7,24 @@ async function research(req: Request, res: Response) {
   const research = req.body as ResearchType
   // Find locations matching the city
   const locations = await Location.find({
-    city: { $regex: research.place.toString() },
+    price: { $lte: research.maxPrice ?? Number.MAX_VALUE },
+    city: { $regex: research.place?.toString() ?? "" },
   })
 
   // Find reservations that overlap with the checkIn and checkOut interval
   const reservations = await Reservation.find({
-    location: { $in: locations.map((location) => location._id.toString()) },
-    $and: [
-      research.maxPrice ? { price: { $lte: research.maxPrice } } : {},
+    $or: [
       {
-        $or: [
-          {
-            $and: [
-              { start: { $lt: research.checkOut } },
-              { end: { $gt: research.checkIn } },
-            ],
-          },
-          {
-            $and: [
-              { start: { $gte: research.checkIn, $lte: research.checkOut } },
-            ],
-          },
-          {
-            $and: [
-              { end: { $gte: research.checkIn, $lte: research.checkOut } },
-            ],
-          },
+        $and: [
+          { start: { $lt: research.checkOut } },
+          { end: { $gt: research.checkIn } },
         ],
+      },
+      {
+        $and: [{ start: { $gte: research.checkIn, $lte: research.checkOut } }],
+      },
+      {
+        $and: [{ end: { $gte: research.checkIn, $lte: research.checkOut } }],
       },
     ],
   })
@@ -46,17 +37,24 @@ async function research(req: Request, res: Response) {
     ),
   )
 
+  if (!research.checkIn || !research.checkOut) {
+    return res.status(500).send()
+    // TODO:
+  }
+
   const days = new Date(
-    research.checkIn.getTime() - research.checkOut.getTime(),
+    research.checkIn?.getTime() - research.checkOut?.getTime(),
   ).getDay()
-  locationsWithoutReservations.map((location) => {
-    return {
-      location,
-      totalPrice: location.price * days,
-      checkIn: research.checkIn,
-      checkOut: research.checkOut,
-    } satisfies OfferType
-  })
+  return res.send(
+    locationsWithoutReservations.map((location) => {
+      return {
+        location,
+        totalPrice: location.price * days,
+        checkIn: research.checkIn!,
+        checkOut: research.checkOut!,
+      } satisfies OfferType
+    }),
+  )
 }
 
 export const researchRouter = Router()
