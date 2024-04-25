449b254109ca549cb9939249e1d70063c2a37d9a
Sébastien VIAL <sebastien@shyrogan.fr>
2024-04-25 09:37:22 +0200

    désolé pablo...

--- src/routes/research.ts
+++ src/routes/research.ts
@@ -22,6 +22,9 @@ async function research(req: Request, res: Response) {
     },
     research.maxPrice ? { price: { $lte: research.maxPrice / days } } : {},
   ])
+  if (research.maxPrice) {
+    console.log("jours: ", days, " prix par nuit:", research.maxPrice / days)
+  }
   const locations = await locationQuery
 
   // Find reservations that overlap with the checkIn and checkOut interval
